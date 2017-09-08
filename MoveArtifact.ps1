993740c166656d4d314d1d7de02a7b5172063d1c
Jonas Vinther <jrvinther@gmail.com>
2017-09-08 14:49:41 +0200

    Test powershell

--- MoveArtifact.ps1
+++ MoveArtifact.ps1
@@ -3,16 +3,25 @@ param(
     [ValidateRange(0,[int]::MaxValue)]
     [int] $build_number = $(Throw "Please specify build number"),
     
-    [Parameter(Position=1)]
+   [Parameter(Position=1)]
     [ValidateSet('P','S','T')]
     [string] $from,
     
-    [Parameter(Position=2)]
+   [Parameter(Position=2)]
     [ValidateSet('P','S','T')]
     [string] $to,
 
     [Parameter(Position=3)]
-    [string] $username
+    [string] $username,
+
+    [Parameter(Position=4)]
+    [string] $password,
+
+    [Parameter(Position=5)]
+    [string] $artifactoryApiPath,
+    
+    [Parameter(Position=6)]
+    [string] $repository
 )
 
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(('{0}:{1}' -f $username,$password)))
